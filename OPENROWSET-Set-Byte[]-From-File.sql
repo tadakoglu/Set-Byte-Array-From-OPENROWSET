0ecf4b2289561df79302232719cc3ebdd5a7c116
Tayfun Adakoğlu <tadakoglu@gmail.com>
2019-03-15 17:44:40 +0300

    Update OPENROWSET-Set-Byte[]-From-File.sql

--- OPENROWSET-Set-Byte[]-From-File.sql
+++ OPENROWSET-Set-Byte[]-From-File.sql
@@ -1,1 +1,1 @@
-﻿UPDATE Products SET ResimVerisi=(SELECT * FROM   OPENROWSET(BULK N'C:\15.jpg', SINGLE_BLOB) rs) where Products.UrunID=15
+﻿UPDATE Products SET ResimVerisi=(SELECT * FROM   OPENROWSET(BULK N'C:\15.jpg', SINGLE_BLOB) rs) where Products.UrunID=15;
